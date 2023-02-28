b64e6c384817160440df77de34e45e5ae2edc4d3
citrouille <clement.trouillard@oscaro.com>
2023-02-28 10:41:37 +0100

    lein: added marginalia plugin

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/iomonad/argoj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :plugins [[lein-marginalia "0.9.1"]]
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-http            "3.12.3"]
                  [cheshire            "5.11.0"]]
