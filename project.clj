fb17bb15fd6c5bed42b971ef3f693aedeccda372
citrouille <clement.trouillard@oscaro.com>
2023-02-28 10:58:12 +0100

    added deploy repos

--- project.clj
+++ project.clj
@@ -3,6 +3,14 @@
   :url "https://github.com/iomonad/argoj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :deploy-repositories [["snapshots" {:url "https://repo.clojars.org"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]
+                        ["releases"  {:url "https://repo.clojars.org"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]]
   :plugins [[lein-marginalia "0.9.1"]]
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-http            "3.12.3"]
